c4d7b3abe25283b696067a0e4081c37df2e65738
jlee341 <jlee341@uncc.edu>
2023-12-08 23:28:19 -0500

    activity 14

---
+++ itis3135/scripts/Lee-Activity14.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $("#nav_list li").click(function() {
+ 
+        var title = $(this).children("a").attr("title");
+ 
+        $.get(title + ".json", function(data, status) {
+ 
+         data = data['speakers'][0];
+ 
+   $("main h1").html(data['title']);
+ 
+   $("main h2").html(data['month']+"<br />"+data['speaker']);
+ 
+   $("main img").attr("src", data.image);
+ 
+   $("main p").html(data.text);
+ 
+        });
+ 
+    });
+ 
+ });

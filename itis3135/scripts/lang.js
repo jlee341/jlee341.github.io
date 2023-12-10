8c2a3e3e4ac34b0335d0aa69f3cf400aa14f8a7a
jlee341 <jlee341@uncc.edu>
2023-12-09 21:18:46 -0500

    work

--- itis3135/scripts/lang.js
+++ itis3135/scripts/lang.js
@@ -7,34 +7,59 @@ $(document).ready(function() {
             success: function(data) {
                 // Assuming data is an array of providers with supported languages
                 // Update your HTML with the supported languages information
-                var providersHtml = '';
+                
 
-                data.forEach(function(provider) {
-                    providersHtml += '<h2>' + provider.display_name + '</h2>';
-                    providersHtml += '<p>Provider Type: ' + provider.provider_type + '</p>';
-                    providersHtml += '<p>Update Rate: ' + provider.update_rate + ' seconds</p>';
-                    providersHtml += '<p>Supports Tags: ' + provider.supports_tags + '</p>';
-                    
-                    if (provider.supported_languages && provider.supported_languages.length > 0) {
-                        providersHtml += '<h3>Supported Languages</h3>';
-                        providersHtml += '<ul>';
-                        
-                        provider.supported_languages.forEach(function(language) {
-                            providersHtml += '<li>' + language.name + ' - ' + language.code + '</li>';
-                        });
-
-                        providersHtml += '</ul>';
-                    }
+                data.forEach(function(provider) {      
+                    var template = `
+    <section class="bot">
+    <div>
+    <h2>Provider Name: ${provider.name}</h2>
+    <p>Provider Type: ${provider.provider_type}</p>
+    <p>Update Rate: ${provider.update_rate}</p>
+    <p>Supports Tags: ${provider.supports_tags}</p>
+    <ul>
+    ${provider.supported_languages.map((s) => "<li>" + s.name + " - " + s.code + "</li>").join("")}
+    </ul>
+    </div>
+    </section>
+    `; 
+    $("#content").append(template)
                 });
-
-                $('#content').html(providersHtml);
             },
             error: function() {
                 $('#content').html('<p>Error loading supported languages.</p>');
             }
         });
     }
-
     // Initial load of supported languages
     loadSupportedLanguages();
+    function loadVersionInfo() {
+        $.ajax({
+            url: 'https://beta-news.snailbot.net/api/version',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                // Assuming data is an array of providers with supported languages
+                // Update your HTML with the supported languages information
+                
+
+                      
+                    var template = `
+    <section class="bot">
+    <div>
+    <h1>Version Info</h1>
+    <p>Version: ${data.version}</p>
+    <p>Status: ${data.status}</p>
+    </div>
+    </section>
+    `; 
+    $("#content").append(template)
+                
+            },
+            error: function() {
+                $('#content').html('<p>Error loading supported languages.</p>');
+            }
+        });
+    }
+    loadVersionInfo();
 });

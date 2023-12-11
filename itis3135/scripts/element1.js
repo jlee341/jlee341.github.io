bf5028dd5db461b9960a25a5bc2440ba650a58f3
jlee341 <jlee341@uncc.edu>
2023-12-10 20:10:32 -0500

    scripts

---
+++ itis3135/scripts/element1.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+    strings: ['Hello there! Thanks for visiting the SnailBot Documentation. Here you will find everything you need to know about the bot. SnailBot is a public bot, which means you can freely add it and remove it from your own server using the link below.'],
+    typeSpeed: 10,
+  });
---
+++ itis3135/scripts/element2.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element2', {
+    strings: ['Meet SnailBot! -- The best Discord bot for displaying the latest news from War Thunder, Crossout, Steam games and more. SnailBot will keep your server updated with the latest news from your favorite games, and setup is super, super easy! With the use of a single command, SnailBot will guide you through the process of setting up a feed for your server. You can even setup multiple ones if you want, and in different languages as well!'],
+    typeSpeed: 10,
+  });
